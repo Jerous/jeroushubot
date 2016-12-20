2900dda7a39ec5c7749904fd68babbe9d132841c
Shingo <shingo0620@gmail.com>
2016-12-20 15:49:51 +0800

    Change the shuffle function

--- scripts/clear.js
+++ scripts/clear.js
@@ -5,7 +5,16 @@ module.exports = function (robot) {
   var finalJob = [];
   var tempJob = [];
   var count = 0;
-  
+
+  Array.prototype.shuffle = function () {
+    this.forEach(function (el, index, origin) {
+      var randomIndex = Math.floor(Math.random()*origin.length)
+      var cache = origin[index]
+      origin[index] = origin[randomIndex]
+      origin[randomIndex] = cache
+    })
+  }
+
   Array.prototype.allValuesEqual = function() {
     var count = 0;
     for(var i = 1; i < this.length; i++)
@@ -15,7 +24,7 @@ module.exports = function (robot) {
     }
     return count;
   }
-  
+
   robot.hear(/開始抽打掃/, function (res) {
     res.send('@channel 開始抽打掃囉，「好手氣」電腦幫大家抽');
     finalJob = [];
@@ -23,32 +32,32 @@ module.exports = function (robot) {
     userGetNow = null;
     count = 0;
   });
-  
+
   robot.hear(/好手氣/, function (res) {
     function shuffle(job,finalJob) {
       var num = Math.random() > 0.5 ? -1:1;
       return num;
     }
-    finalJob = job.sort(shuffle);
-    for (var i = 0; i < finalJob.length; i++) {
-      res.send(people[i] + '：' + finalJob[i]);
+    job.shuffle();
+    for (var i = 0; i < job.length; i++) {
+      res.send(people[i] + '：' + job[i]);
     }
   });
-  
+
   /*robot.hear(/我抽/, function (res) {
     count = count + 1;
     var userGetNow = res.random(job);
     tempJob.push(userGetNow);
     finalJob.push(res.message.user.name + ' ' + userGetNow);
     res.reply(res.message.user.name + ' ' + userGetNow);
-    
+
     if (count == 4 && tempJob.allValuesEqual() !== 0) {
       res.send('工作有重複喔，請重複的人重抽！！！');
       count = count - tempJob.allValuesEqual() - 1;
     }
-    
+
   });
-  
+
   robot.hear(/結果/, function (res) {
     if (tempJob.allValuesEqual() == 0) {
       for (var i = 0; i < finalJob.length; i++) {
@@ -58,6 +67,6 @@ module.exports = function (robot) {
       res.send('還有人沒抽完阿，不然就是工作重複，猴急什麼！！？');
     }
   });*/
-  
+
 
 }
