6870866aaa18ab3f182fe36eeea77b28e3c178e1
Jerous <tlu37317@gmail.com>
2018-07-20 16:23:58 +0800

    不要再cleaning裡面講話

--- scripts/tuling123.js
+++ scripts/tuling123.js
@@ -4,23 +4,25 @@ const config = require('../bin/config.json');
 module.exports = function (robot) {
 
   robot.hear(/(.*)/, function(res) {
-    var selfRes = res;
-    var keyWord = res.match[1];
-    var data = JSON.stringify({
-      reqType:0,
-        perception: {
-          inputText: {
-            text: keyWord
-          }
-        },
-        userInfo: config.tuling123
-    })
-    robot.http("http://openapi.tuling123.com/openapi/api/v2")
-      .header('Content-Type', 'application/json')
-      .post(data)(function(err, res, body) {
-        var body = JSON.parse(body);
-        selfRes.send(opencc.simplifiedToTaiwanWithPhrases(body.results[0].values.text));
-    });
+    if (res.envelope.room !== 'C0Q32Q4GZ') {
+      var selfRes = res;
+      var keyWord = res.match[1];
+      var data = JSON.stringify({
+        reqType:0,
+          perception: {
+            inputText: {
+              text: keyWord
+            }
+          },
+          userInfo: config.tuling123
+      })
+      robot.http("http://openapi.tuling123.com/openapi/api/v2")
+        .header('Content-Type', 'application/json')
+        .post(data)(function(err, res, body) {
+          var body = JSON.parse(body);
+          selfRes.send(opencc.simplifiedToTaiwanWithPhrases(body.results[0].values.text));
+      });
+    }
   });
 
 }
